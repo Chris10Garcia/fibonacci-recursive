c89d8a944943142528c4ae69c77c3b8285d81035
Christian Garcia <bones192nd@gmail.com>
2023-08-26 16:26:35 -0400

    Used JS to solve algo

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,51 @@
+/* 
+
+Going to brush up on my JS and solve it using this language
+ 
+
+the way the fib sequence works
+
+0 1 1 2 3 5 8 13 21 34 55 ...
+
+0 1 2 3 4 5 6  7  8  9 10
+
+basecase,
+
+if index 0 return 0
+if index 1 return 1
+else ...
+  you want to get down all the way to 1 + 0. This is how the sequence starts. 
+  To do this, you're starting at index 2... from there, 
+  you can do func(n-1) + func(n-2) you can just keep adding the return value of one, 
+  which will go
+index value[n-2] + value[n-1], answer
+0   return 0
+1   return 1     # or 1 + 0 
+2   1  +  1, 2
+3   2  +  1, 3
+4   3  +  2, 5
+5   5  +  3, 8
+6   8  +  5, 13
+7   13 +  8, 21
+8   21 + 13, 34
+9   34 + 21, 55
+
+
+*/
+
 function fibonacci(n) {
-  // type your code here
+  
+  
+  if (n === 0){
+    return 0
+  }
+  
+  if (n === 1){
+    return 1
+  }
+  else {
+    return fibonacci(n-1) + fibonacci(n-2)
+  }
 }
 
 if (require.main === module) {
